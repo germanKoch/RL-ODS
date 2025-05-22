f134f38f66f3fcb059ab255f1404ccb3847e6a95
germankochnev <kochgerm@gmail.com>
2025-05-22 21:34:56 +0300

    requirements.txt enriched

--- module1/1_taxi_dz.py
+++ module1/1_taxi_dz.py
@@ -1,14 +1,19 @@
 import gymnasium as gym
+import time
 
-env = gym.make("Taxi-v3")
+env = gym.make("Taxi-v3", render_mode="human")
 env.action_space.seed(42)
 
 
 observation, info = env.reset(seed=42)
 
 for _ in range(1000):
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    
+    env.render()
+    print("OBSERAVTION: ", observation, " REWARD: ", reward, " ACTION: ", action)
+    time.sleep(0.1)
     if terminated or truncated:
         observation, info = env.reset()
 
